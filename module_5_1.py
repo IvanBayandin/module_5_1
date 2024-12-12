1cb098226bf5db5c0985e71257b28f3b0d5f22ad
Иван <v.bayandin2016@outlook.com>
2024-12-13 03:54:51 +0500

    Module_5_1add

--- module_5_1.py
+++ module_5_1.py
@@ -1,6 +1,6 @@
 class House:
     def __init__(self, name, number_of_floors):
-        self.nams = name
+        self.name = name
         self.number_of_floors = number_of_floors
     def go_to(self, new_floor):
         new_floor = int(new_floor)
@@ -11,13 +11,21 @@ class House:
             while new_floor > floor:
                 floor += 1
                 print(floor)
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
 
-h3 = House('9-этажное здание', 9)
-h3.go_to(7)
-h3.go_to(99)
+# h1 = House('ЖК Горский', 18)
+# h2 = House('Домик в деревне', 2)
+# h1.go_to(5)
+# h2.go_to(10)
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(h1)
+print(h2)
+print(len(h1))
+print(len(h2))
