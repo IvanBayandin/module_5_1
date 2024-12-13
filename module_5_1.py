8a4d1753e7aebebecbbcd02b73dabe43e0b715a2
Иван <v.bayandin2016@outlook.com>
2024-12-13 19:36:06 +0500

    Module_5_3 operator overloading

--- module_5_1.py
+++ module_5_1.py
@@ -2,6 +2,7 @@ class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
     def go_to(self, new_floor):
         new_floor = int(new_floor)
         floor = 0
@@ -11,11 +12,41 @@ class House:
             while new_floor > floor:
                 floor += 1
                 print(floor)
+
     def __len__(self):
         return self.number_of_floors
+
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
+    def __eq__(self, other):
+        return self.number_of_floors == other
+
+    def __lt__(self, other):
+        return self.number_of_floors < other
+
+    def __le__(self, other):
+        return self.number_of_floors <= other
+
+    def __gt__(self, other):
+        return self.number_of_floors > other
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other
+
+    def __ne__(self, other):
+        return self.number_of_floors != other
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
 
 
 # h1 = House('ЖК Горский', 18)
@@ -23,9 +54,27 @@ class House:
 # h1.go_to(5)
 # h2.go_to(10)
 
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+# print(h1)
+# print(h2)
+# print(len(h1))
+# print(len(h2))
+
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
-print(len(h1))
-print(len(h2))
+print(h1 == h2)  # __eq__
+h1 = h1 + 10  # __add__
+print(h1)
+print(h1 == h2)
+h1 += 10  # __iadd__
+print(h1)
+h2 = 10 + h2  # __radd__
+print(h2)
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
